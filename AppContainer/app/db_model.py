9e251257a80affd8585776966a2ae4c9ba865027
Robert Warden-Rothman <rob.wr@grobio.com>
2023-10-30 10:15:48 -0400

    Add PlasmidSeqAssembly and related database models
    This commit introduces three new database models namely PlasmidSeqAssembly, AssemblyFeature, and AssemblyPolymorphism. These models aim to represent multiple assembly output options, automated genotype correction, and plasmid extraction from gfa files, respectively.
    
    Also, it includes an improvement for the PlasmidSeqRun model by adding 'assembly count' and 'assemblies' fields for capturing more detailed information about the associated assemblies. Finally, if-main-logic was included at the end of the file to allow independent execution, facilitating the creation of the described tables in the database.
    
    These changes contribute to broadening the program's functionality and handling the complexity of multiple assembly options better.
    
    Signed-off-by: Robert Warden-Rothman <rob.wr@grobio.com>

--- AppContainer/app/db_model.py
+++ AppContainer/app/db_model.py
@@ -26,15 +26,61 @@ class PlasmidSeqRun(LambdaMixin, table=True):
     last_step: Optional[str]
     template_name: Optional[str]
     basespace_href: Optional[str]
+    assembly_count: int = Field(default=0)
     error_type: Optional[str]
     error_message: Optional[str]
     error_path: Optional[str]
+
+    assemblies: List["PlasmidSeqAssembly"] = Relationship(back_populates='sample')
 
     def data_path(self, folder: str = None) -> str:
         path = f"{self.experiment_id}/{self.data_id}"
         if folder:
             path += '/' + folder
         return path
+
+
+class PlasmidSeqAssembly(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    run_id: int = Field(foreign_key='plasmidseqrun.id')
+    assembly_name: str
+    contig_path: str
+    length: int
+
+    sample: PlasmidSeqRun = Relationship(back_populates='assemblies')
+    features: List["AssemblyFeature"] = Relationship(back_populates='assembly')
+    polymorphisms: List["AssemblyPolymorphism"] = Relationship(back_populates='assembly')
+
+
+class FeaturePolymorphism(SQLModel, table=True):
+    feature_id: Optional[int] = Field(default=None, foreign_key='assemblyfeature.id', primary_key=True)
+    polymorphism_id: Optional[int] = Field(default=None, foreign_key='assemblypolymorphism.id', primary_key=True)
+
+
+class AssemblyFeature(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    assembly_id: int = Field(foreign_key='plasmidseqassembly.id')
+    wt_feature_name: str
+    assembly_feature_name: str
+    deleted: bool = Field(default=False)
+    frameshift_residue: Optional[int]
+
+    assembly: PlasmidSeqAssembly = Relationship(back_populates='features')
+    polymorphisms: List["AssemblyPolymorphism"] = Relationship(back_populates='features',
+                                                               link_model=FeaturePolymorphism)
+
+
+class AssemblyPolymorphism(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    assembly_id: int = Field(foreign_key='plasmidseqassembly.id')
+    wt_nt_start: int
+    wt_nt_end: int
+    assembly_nt_start: int
+    assembly_nt_end: int
+    cds_effect: Optional[str]
+
+    assembly: PlasmidSeqAssembly = Relationship(back_populates='features')
+    features: List["AssemblyFeature"] = Relationship(back_populates='polymorphisms', link_model=FeaturePolymorphism)
 
 
 class MapStatusInfo(SQLModel):
@@ -56,3 +102,7 @@ def make_engine():
     engine = create_engine(os.environ['DB_URI'])
     print('Engine Created')
     return engine
+
+
+if __name__ == '__main__':
+    SQLModel.metadata.create_all(make_engine())
